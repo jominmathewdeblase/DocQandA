28471b238cec79b3f189d92058d1e6b9ebde6542
jominmathewdeblase <jominm09@gmail.com>
2024-10-14 20:08:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def vector_embedding():
     if "vectors" not in st.session_state:
 
         st.session_state.embeddings=GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-        st.session_state.loader=PyPDFDirectoryLoader("./deblase_doc_c") ## Data Ingestion
+        st.session_state.loader=PyPDFDirectoryLoader("./debase_doc_c") ## Data Ingestion
         st.session_state.docs=st.session_state.loader.load() ## Document Loading
         st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200) ## Chunk Creation
         st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:20]) #splitting
